c598572f1984cae477bad3b057e81846d6a31f7d
Alexander Volz <Alexander@volzit.de>
2021-12-06 19:52:20 +0100

    changed to sqlite

--- NetEvent/Server/Program.cs
+++ NetEvent/Server/Program.cs
@@ -9,8 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString, o=>o.EnableRetryOnFailure()));
+    options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
