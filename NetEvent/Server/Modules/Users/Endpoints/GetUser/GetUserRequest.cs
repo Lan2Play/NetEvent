6c574a2c41b6d2b6ec529567a6cb479a3b70c7f0
Andreas Hahn <andyhahn91@gmail.com>
2022-04-19 21:49:46 +0200

    Get UserHandler

---
+++ NetEvent/Server/Modules/Users/Endpoints/GetUser/GetUserRequest.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using NetEvent.Shared.Models;
+
+namespace NetEvent.Server.Modules.Users.Endpoints
+{
+    public class GetUserRequest : IRequest<GetUserResponse>
+    {
+        public GetUserRequest(string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get; }
+    }
+}
