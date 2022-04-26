91041510c772c3e35c06c9e4937ba5ab774b9fd0
Andreas Hahn <andyhahn91@gmail.com>
2022-04-26 23:57:32 +0200

    Make Properties nullable

--- NetEvent/Shared/Dto/OrganizationData.cs
+++ NetEvent/Shared/Dto/OrganizationData.cs
@@ -1,11 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace NetEvent.Shared.Dto
+﻿namespace NetEvent.Shared.Dto
 {
     public class OrganizationData
     {
-        public string Key { get; set; }
+        public string? Key { get; set; }
 
-        public string Value { get; set; }
+        public string? Value { get; set; }
     }
 }
