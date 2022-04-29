5c7d5c0bb6758ba4d8fd49a3c82b6772c4e75bdc
Alexander Volz <Alexander@volzit.de>
2022-04-29 16:08:04 +0200

    removed test dasboard usage

--- NetEvent/Client/Pages/Administration/Index.razor.cs
+++ NetEvent/Client/Pages/Administration/Index.razor.cs
@@ -7,21 +7,16 @@ namespace NetEvent.Client.Pages.Administration
     public partial class Index 
     {
         [Inject]
-        private IOrganizationDataService OrganizationDataService { get; set; } = default!;
-
-        [Inject]
         private IUserService UserService { get; set; } = default!;
 
         public List<UserDto>? Users { get; private set; }
         
-        public List<OrganizationDataDto>? OrganizationData { get; private set; }
 
         protected override async Task OnInitializedAsync()
         {
             using var cancellationTokenSource = new CancellationTokenSource();
             
             Users = await UserService.GetUsersAsync(cancellationTokenSource.Token).ConfigureAwait(false);
-            OrganizationData = await OrganizationDataService.GetOrganizationDataAsync(cancellationTokenSource.Token).ConfigureAwait(false);
         }
 
     }
