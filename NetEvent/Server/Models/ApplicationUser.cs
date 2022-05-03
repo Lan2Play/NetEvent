5fa3efc07f8805d8e3cbe5c09eeb3d4eb517428c
Andreas Hahn <andyhahn91@gmail.com>
2022-05-03 20:56:34 +0200

    Fix Warnings

--- NetEvent/Server/Models/ApplicationUser.cs
+++ NetEvent/Server/Models/ApplicationUser.cs
@@ -4,12 +4,11 @@ namespace NetEvent.Server.Models;
 
 public class ApplicationUser : IdentityUser
 {
-    private string test;
-
     public string FirstName { get; set; }
 
     public string LastName { get; set; }
 
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1011:Closing square brackets should be spaced correctly", Justification = "<Pending>")]
     public byte[]? ProfilePicture { get; set; }
 
     public override string PasswordHash { get => base.PasswordHash; set => base.PasswordHash = value; }
