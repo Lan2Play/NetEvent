55a990ab0b271b4893a1ab6c125601aa83f5836c
MD-V <michael@volz.dev>
2022-05-11 22:47:05 +0200

    Fix code smells

--- NetEvent/Shared/Dto/LoginRequestDto.cs
+++ NetEvent/Shared/Dto/LoginRequestDto.cs
@@ -7,11 +7,11 @@ public class LoginRequestDto
 {
     [Required(ErrorMessage = "UserName is required.")]
     [JsonPropertyName("username")]
-    public string UserName { get; set; }
+    public string UserName { get; set; } = default!;
 
     [Required(ErrorMessage = "Password is required.")]
     [JsonPropertyName("password")]
-    public string Password { get; set; }
+    public string Password { get; set; } = default!;
 
     [JsonPropertyName("rememberme")]
     public bool RememberMe { get; set; } = true;
