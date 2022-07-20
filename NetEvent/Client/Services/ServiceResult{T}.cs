a4d1ba8c5b9c337084f2c1d6cfa6de68a964b300
TheR00st3r <andyhahn91@gmail.com>
2022-07-20 20:49:31 +0200

    Ignore ClientService Test Coverage

--- NetEvent/Client/Services/ServiceResult{T}.cs
+++ NetEvent/Client/Services/ServiceResult{T}.cs
@@ -1,5 +1,8 @@
-﻿namespace NetEvent.Client.Services
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace NetEvent.Client.Services
 {
+    [ExcludeFromCodeCoverage(Justification = "Ignore UI Services")]
     public class ServiceResult<T> : ServiceResult
     {
         private ServiceResult(T? data, bool successful, string? messageKey) : base(successful, messageKey)
