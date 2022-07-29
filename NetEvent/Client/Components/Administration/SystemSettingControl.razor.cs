79c15dc7d4349cdac097d31754725c5172b63cc5
TheR00st3r <andyhahn91@gmail.com>
2022-07-30 00:39:32 +0200

    Resolve Warnings

--- NetEvent/Client/Components/Administration/SystemSettingControl.razor.cs
+++ NetEvent/Client/Components/Administration/SystemSettingControl.razor.cs
@@ -1,25 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
-using System.Net.Http;
-using System.Net.Http.Json;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.Web.Virtualization;
-using Microsoft.AspNetCore.Components.WebAssembly.Http;
-using Microsoft.JSInterop;
-using MudBlazor;
-using NetEvent.Client;
-using NetEvent.Client.Shared;
-using NetEvent.Client.Components;
-using NetEvent.Shared.Config;
 using NetEvent.Client.Services;
+using NetEvent.Shared.Config;
 using NetEvent.Shared.Dto;
-using System.Threading;
 
 namespace NetEvent.Client.Components.Administration
 {
--- NetEvent/Shared/Config/SystemSettingBuilder.cs
+++ NetEvent/Shared/Config/SystemSettingBuilder.cs
@@ -8,6 +8,7 @@
         }
 
         public static SystemSettingBuilder OrganizationBuilder { get; } = new SystemSettingBuilder(SystemSettingGroup.OrganizationData);
+
         public static SystemSettingBuilder AuthenticationBuilder { get; } = new SystemSettingBuilder(SystemSettingGroup.AuthenticationData);
 
         public SystemSettingGroup SettingGroup { get; }
